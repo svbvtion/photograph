20c8e43b83fb0a33b7ac223df0efc0643f37123f
MyLittlePonyFan <F-S1de@yandex.ru>
2020-10-28 16:39:21 +0300

    responsive

--- src/static/js/other.js
+++ src/static/js/other.js
@@ -3,4 +3,4 @@
 const contentOther = document.querySelector('.content__other');
 const hero = document.querySelector('.hero');
 
-contentOther.style.marginTop = hero.offsetHeight + 'px'
+contentOther.style.marginTop = hero.offsetHeight + 50 + 'px'
