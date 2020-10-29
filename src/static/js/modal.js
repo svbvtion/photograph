93b03b895e6de1269b081fe8a037c650bfe41ec0
MyLittlePonyFan <F-S1de@yandex.ru>
2020-10-29 14:21:37 +0300

    complete

--- src/static/js/modal.js
+++ src/static/js/modal.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const modalLinks = document.querySelectorAll('.modal__link');
+const modal = document.querySelector('.modal');
 
 modalLinks.forEach(link => {
 	link.addEventListener('click', function(e) {
@@ -9,50 +10,21 @@ modalLinks.forEach(link => {
 	});
 });
 
-
-function createModal() {
-	body.insertAdjacentHTML('beforeend', `
-		<div class="modal active" id="modal">
-	      <div class="modal__overlay" data-close="true">
-	        <div class="modal__window"><span class="modal__close" data-close="true">&times</span>
-	          <div class="modal__body">
-	            <div class="modal__item modal__content">
-	              <h2 class="modal__title title">Связаться</h2>
-	              <form class="form">
-	                <div class="form__field">
-	                  <input class="form__input" type="text" placeholder="Ваше имя" required>
-	                </div>
-	                <div class="form__field">
-	                  <input class="form__input" type="email" placeholder="Ваш Email" required>
-	                </div>
-	                <div class="form__field">
-	                  <input class="form__input" type="tel" placeholder="Ваш телефон" required>
-	                </div>
-	                <div class="form__submit">
-	                  <button class="form__button">Отправить<span class="form__button_decor"></span></button>
-	                </div>
-	              </form>
-	            </div>
-	            <div class="modal__decor modal__item">
-	              <div class="modal__bg">
-	                <picture>
-	                  <source srcset="static/img/card-img_2.webp" type="image/webp"/><img class="modal__pic" src="static/img/card-img_2.png" alt="card"/>
-	                </picture>
-	              </div>
-	            </div>
-	          </div>
-	        </div>
-	      </div>
-	    </div>
-		`);	
-
-	body.classList.add('lock');
-
-	const modal = document.querySelector('.modal');
-	modal.addEventListener('click', function(e) {
+modal.addEventListener('click', function(e) {
 		if(e.target.dataset.close){
-			modal.parentNode.removeChild(modal);
+			console.log('obj');
+			modal.classList.remove('active')
 			body.classList.remove('lock');
+			body.style.marginRight = 0;
+
 		}
 	});
+
+function createModal() {
+
+	let scrollWidth = window.innerWidth - body.offsetWidth;
+
+	body.style.marginRight = scrollWidth + 'px'
+	modal.classList.add('active')
+	body.classList.add('lock');
 }
